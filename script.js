79f34b815473792f263adcb4d101ad9b88545f79
jadebugnos <173978908+jadebugnos@users.noreply.github.com>
2024-09-29 09:49:54 +0800

    add function that will create the divs

---
+++ script.js
@@ -0,0 +1,20 @@
+const parentBox = document.querySelector('#box-container');
+const numberofBoxes = 16 * 16;
+const originalChild = document.createElement('div');
+originalChild.classList.add('boxes')
+
+
+function multiplyBox() {
+    for (let i = 1; i <= numberofBoxes; i++) {
+        let clones = originalChild.cloneNode(true);
+        clones.classList.add('boxes');
+        parentBox.appendChild(clones);
+    }
+
+    const childrenBoxes = document.getElementsByClassName('boxes');
+    for (child of childrenBoxes) {
+        child.style.backgroundColor = "black";
+    }
+}
+
+multiplyBox()
