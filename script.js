818f525de30541c50c5e2a0f40ca21d08203fc33
jadebugnos <173978908+jadebugnos@users.noreply.github.com>
2024-09-30 10:36:57 +0800

    add input validation and fix bugs
    -delete previous value of boxes to prevent
    overflow
    -convert nodelist to array

--- script.js
+++ script.js
@@ -2,34 +2,37 @@ const parentBox = document.querySelector('#box-container');
 let numberofBoxes = 16 * 16;
 const originalChild = document.createElement('div');
 originalChild.classList.add('boxes');
-
+const toCovert = input => (100 / +input) + '%'; // converts human input into a percentage.
 let columns = "";
 let rows = "";
-
-// converts human input into a value to be added to the grid length rows/column;
-const toCovert = input => (100 / +input) + '%';
-
 
 
 function getUserInput(event) {
     event.preventDefault();
-
     columns = document.getElementById('columns').value;
     rows = document.getElementById('rows').value;
-    numberofBoxes = columns * rows;
+    parentBox.replaceChildren();
+
+    if (columns > 100 ||
+        rows > 100 ||
+        columns <= 0 ||
+        rows <= 0 ||
+        isNaN(columns) ||
+        isNaN(rows)) {
+        alert("invalid input! only accepts values between 1 to 100 positive numbers.")
+        numberofBoxes = 16 * 16;
+    } else {
+        numberofBoxes = columns * rows;
+    }
+
     multiplyBox(numberofBoxes);
-    console.log(numberofBoxes);
-   
-    [...parentBox].forEach(box => {
+    [...parentBox.children].forEach(box => {
         box.style.height = toCovert(columns);
         box.style.width = toCovert(rows);
     })
 }
 
 document.getElementById('user-input').addEventListener('submit', getUserInput);
-
-
-
 
 
 function multiplyBox(boxes) {
@@ -40,7 +43,6 @@ function multiplyBox(boxes) {
     }
 }
 
-multiplyBox(numberofBoxes);
 
 parentBox.addEventListener('mouseover', e => {
     if (e.target !== e.currentTarget) {
@@ -53,4 +55,4 @@ parentBox.addEventListener('mouseover', e => {
 });
 
 
-
+multiplyBox(numberofBoxes);
