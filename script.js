da73d8f031a437ce623c918308f439d1c9053c7d
jadebugnos <173978908+jadebugnos@users.noreply.github.com>
2024-09-30 09:19:17 +0800

    improve getUserInput function

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 const parentBox = document.querySelector('#box-container');
-const numberofBoxes = 16 * 16;
+let numberofBoxes = 16 * 16;
 const originalChild = document.createElement('div');
-originalChild.classList.add('boxes')
-
-// converts human input into a value to be added to the grid length rows/column;
-const toCovert = input => Math.floor(+input / 100);
-
-
-
+originalChild.classList.add('boxes');
 
 let columns = "";
 let rows = "";
+
+// converts human input into a value to be added to the grid length rows/column;
+const toCovert = input => (100 / +input) + '%';
+
+
+
 function getUserInput(event) {
     event.preventDefault();
 
     columns = document.getElementById('columns').value;
     rows = document.getElementById('rows').value;
+    numberofBoxes = columns * rows;
+    multiplyBox(numberofBoxes);
+    console.log(numberofBoxes);
+   
+    [...parentBox].forEach(box => {
+        box.style.height = toCovert(columns);
+        box.style.width = toCovert(rows);
+    })
 }
 
 document.getElementById('user-input').addEventListener('submit', getUserInput);
@@ -24,20 +32,19 @@ document.getElementById('user-input').addEventListener('submit', getUserInput);
 
 
 
-function multiplyBox() {
-    for (let i = 1; i <= numberofBoxes; i++) {
+function multiplyBox(boxes) {
+    for (let i = 1; i <= boxes; i++) {
         let clones = originalChild.cloneNode(true);
         clones.classList.add('boxes');
         parentBox.appendChild(clones);
     }
 }
 
-multiplyBox()
+multiplyBox(numberofBoxes);
 
 parentBox.addEventListener('mouseover', e => {
     if (e.target !== e.currentTarget) {
         e.target.style.backgroundColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
-        console.log(toCovert(rows))
     }
 
     setTimeout(() => {
